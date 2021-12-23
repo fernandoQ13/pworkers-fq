0c8e02ed7c3b970c9d6491f507c5e6dc0da41778
fernandoQ13 <fernando_.1998@hotmail.com>
2021-12-22 23:22:32 -0400

    cambio de puerto a una const

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // configuracines
-app.set('port', process.env.PORT || 3000);//posicionamiento del puerto el cual sera establecido por el servidor 
+const port = process.env.PORT || 3000;//posicionamiento del puerto el cual sera establecido por el servidor 
 // en tal caso que no entonces que se ubique en el pueto 3000
 
 app.set('views', path.join(__dirname, 'views')); //estableciendo la direccion donde se escuentra la carpeta views con todas las vistas
@@ -40,6 +40,6 @@ app.use((req, res, next) => {
 
 // servidor escuchando en el puerto este mensaje es en la consola  
 
-app.listen(app.get('port'), () => {
-  console.log('servidor escuchando en el puerto', app.get('port'));
+app.listen(port, () => {
+  console.log('servidor escuchando en el puerto', port);
 });
